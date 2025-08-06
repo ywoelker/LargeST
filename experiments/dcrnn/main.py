ed58cb3e1cf5a91846d513129d88b3b7f8dd3245
ywoelker <ywoelker@gmail.com>
2025-08-06 15:02:05 +0200

    fix: wrong usage of uuid package

--- experiments/dcrnn/main.py
+++ experiments/dcrnn/main.py
@@ -40,7 +40,7 @@ def get_config():
     parser.add_argument('--clip_grad_value', type=float, default=5)
     args = parser.parse_args()
 
-    log_dir = './results/{}/{}/{}_{}/'.format(args.dataset, args.model_name,datetime.now().strftime('%m-%d_%H-%M-%S'), uuid.uuid4[:4])
+    log_dir = './results/{}/{}/{}_{}/'.format(args.dataset, args.model_name,datetime.now().strftime('%m-%d_%H-%M-%S'), str(uuid.uuid4())[-6:])
     logger = get_logger(log_dir, __name__, 'record_s{}.log'.format(args.seed))
     logger.info(args)
     
@@ -81,7 +81,7 @@ def main():
                           dataloader=dataloader,
                           scaler=scaler,
                           sampler=None,
-                          experiment = get_experiment(args.experiment_id),
+                          experiment = get_experiment(args),
                           loss_fn=loss_fn,
                           lrate=args.lrate,
                           optimizer=optimizer,
--- experiments/stgode/main.py
+++ experiments/stgode/main.py
@@ -39,7 +39,7 @@ def get_config():
     parser.add_argument('--clip_grad_value', type=float, default=0)
     args = parser.parse_args()
 
-    log_dir = './results/{}/{}/{}_{}/'.format(args.dataset, args.model_name,datetime.now().strftime('%m-%d_%H-%M-%S'), uuid.uuid4[:4])
+    log_dir = './results/{}/{}/{}_{}/'.format(args.dataset, args.model_name,datetime.now().strftime('%m-%d_%H-%M-%S'), str(uuid.uuid4())[-6:])
     logger = get_logger(log_dir, __name__, 'record_s{}.log'.format(args.seed))
     logger.info(args)
     
@@ -79,7 +79,7 @@ def main():
                         model=model,
                         dataloader=dataloader,
                         scaler=scaler,
-                        experiment = get_experiment(args.experiment_id),
+                        experiment = get_experiment(args),
                         sampler=None,
                         loss_fn=loss_fn,
                         lrate=args.lrate,
