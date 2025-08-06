4e9429f0496499e027ca69a94c96a6b5835c2597
ywoelker <ywoelker@gmail.com>
2025-08-06 13:26:36 +0200

    change: Using a new save_path that goes to ./results and add the timestamp

--- experiments/bigst/preprocess.py
+++ experiments/bigst/preprocess.py
@@ -47,7 +47,7 @@ def get_config():
     
     args = parser.parse_args()
 
-    log_dir = './experiments/{}/{}/'.format(args.model_name, args.dataset)
+    log_dir = './results/{}/{}/{}_{}/'.format(args.dataset, args.model_name,datetime.now().strftime('%m-%d_%H-%M-%S'), uuid.uuid4[:4])
     logger = get_logger(log_dir, __name__, 'record_s{}.log'.format(args.seed))
     logger.info(args)
     
--- experiments/lstm/main.py
+++ experiments/lstm/main.py
@@ -1,6 +1,9 @@
 import os
 import argparse
 import numpy as np
+import uuid
+from datetime import datetime
+
 
 import sys
 sys.path.append(os.path.abspath(__file__ + '/../../..'))
@@ -36,7 +39,7 @@ def get_config():
     parser.add_argument('--clip_grad_value', type=float, default=5)
     args = parser.parse_args()
 
-    log_dir = './experiments/{}/{}/'.format(args.model_name, args.dataset)
+    log_dir = './results/{}/{}/{}_{}/'.format(args.dataset, args.model_name,datetime.now().strftime('%m-%d_%H-%M-%S'), uuid.uuid4[:4])
     logger = get_logger(log_dir, __name__, 'record_s{}.log'.format(args.seed))
     logger.info(args)
     
