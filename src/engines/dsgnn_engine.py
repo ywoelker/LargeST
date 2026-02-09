44ac0b8bcb29e52090aff2549473b0c8b0548245
ywoelker <ywoelker@gmail.com>
2026-02-09 08:48:45 +0100

    adds logging of DSN embeddings.

--- src/engines/dsgnn_engine.py
+++ src/engines/dsgnn_engine.py
@@ -11,6 +11,8 @@ class DSGNN_Engine(BaseEngine):
         self.static_prefilter = static_prefilter
         self.additional_loss_weight = additional_loss_weight
 
+        self.embedding_evolution = {}
+
     def forward(self, X, label, isTrain = False):
 
         if self.static_prefilter is not None:
@@ -35,6 +37,9 @@ class DSGNN_Engine(BaseEngine):
 
         loss = super(DSGNN_Engine, self).loss(pred, label, mask_value, loss_container)
 
+        if pred_dict['assignment_scores_source'] is None or pred_dict['assignment_scores_target'] is None:
+            return loss
+
         mean_source_attention = torch.norm(pred_dict['assignment_scores_source'], p = 1)
         mean_target_attention = torch.norm(pred_dict['assignment_scores_target'], p = 1)
 
@@ -45,5 +50,30 @@ class DSGNN_Engine(BaseEngine):
 
 
         return loss + additional_loss
+    
 
+    def evaluate(self, mode) -> tuple:
+        evaluation_results = super().evaluate(mode)
+
+        if self.epoch in self.embedding_evolution or mode != 'val':
+            return evaluation_results
         
+        self.embedding_evolution[self.epoch] = self.model.context_emb_layer.detach().cpu().numpy()
+
+
+        return evaluation_results
+    
+    def train(self):
+        train_result = super().train()
+
+        for keys in list(self.embedding_evolution.keys()):
+            if keys > self.best_epoch:
+                del self.embedding_evolution[keys]
+
+        embedding_evolution_array = []
+        for epoch in range(self.best_epoch + 1):
+            embedding_evolution_array.append(self.embedding_evolution[epoch])
+
+        np.save(self._save_path + '/embedding_evolution.npy', np.array(embedding_evolution_array))
+
+        return train_result        
